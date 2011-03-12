a3a3ef00bf109fd46fb6dfe1ed794da620fe0bd7
Bruce Duncan <bwduncan@gmail.com>
2011-03-12 19:26:41 +0000

    Compile UI form in setup.py.

--- setup.py
+++ setup.py
@@ -2,16 +2,23 @@
 
 from distutils.core import setup
 from subprocess import Popen, PIPE
+from PyQt4 import uic
+import sys
 
 
 def git_describe(abbrev=4):
-    proc = Popen(['git', 'describe'], stdout=PIPE)
+    proc = Popen(['git', 'describe', '--abbrev=%d' % abbrev], stdout=PIPE)
     return proc.communicate()[0].strip()
+
+if len(sys.argv) > 1 and sys.argv[1] == 'build':
+    with open('cpusliderwindow.py', 'w') as f:
+        uic.compileUi('cpuslider.ui', f)
 
 setup(name="cpuslider",
       description="A simple slider application to control cpufreq in Linux",
       version=git_describe(),
       scripts=['cpuslider.py'],
+      py_modules=['cpusliderwindow'],
       author="Bruce Duncan",
       author_email="bwduncan@gmail.com",
       url="http://github.com/bwduncan/cpuslider",
