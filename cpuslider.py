00b67ec32b2397c00bb349bd670e0cdc279a6151
Bruce Duncan <bwduncan@gmail.com>
2011-03-12 19:13:01 +0000

    Add some logic to the radio buttons (it still doesn't work).
    Try not to redraw the whole layout each time a radio button is pressed...

--- cpuslider.py
+++ cpuslider.py
@@ -2,6 +2,7 @@
 
 from cpusliderwindow import Ui_cpuSliderWindow
 import sys
+from functools import partial
 from PyQt4 import QtGui, QtCore
 
 CPUFREQ = '/sys/devices/system/cpu/cpu0/cpufreq/'
@@ -13,34 +14,48 @@ class CPUSlider(QtGui.QMainWindow):
 
         self.ui = Ui_cpuSliderWindow()
         self.ui.setupUi(self)
-        self.refresh()
+        self.statusLabel = QtGui.QLabel()
+        self.ui.statusbar.addWidget(self.statusLabel)
+        self.refresh(first=True)
 
-    def refresh(self):
+    def refresh(self, first=False):
         with open(CPUFREQ + 'scaling_governor') as f:
             governor = f.read().strip()
-        self.ui.statusbar.addWidget(QtGui.QLabel(governor))
-        with open(CPUFREQ + 'scaling_available_governors') as f:
+        self.statusLabel.setText(governor)
+        if first:
             layout = QtGui.QGridLayout()
+        else:
+            layout = self.ui.groupBox.layout()
+        with open(CPUFREQ + 'scaling_available_governors') as f:
             governors = f.read().split()
-            for i, line in enumerate(governors):
-                button = QtGui.QRadioButton(line)
-                if governor == line:
-                    button.setChecked(True)
-                layout.addWidget(button, i, 0)
-                layout.setRowStretch(i, 1)
-            if governor == "userspace":
-                with open(CPUFREQ + 'scaling_available_frequencies') as f:
-                    self.freqs = f.read().split()
-                with open(CPUFREQ + 'scaling_cur_freq') as f:
-                    cur_freq = f.read().strip()
-                slider = QtGui.QSlider(QtCore.Qt.Vertical)
-                slider.setMaximum(len(self.freqs) - 1)  # minimum defaults to 0
-                layout.addWidget(slider, 0, 1, len(governors) - 1, 1)
-                self.connect(slider, QtCore.SIGNAL('valueChanged(int)'),
-                             self.updateLabel)
+        print layout.children()
+        for button in layout.children():
+            if button.text() not in governors:
+                layout.removeWidget(button)
+        for i, line in enumerate(governors):
+            if line in [x.text() for x in layout.children()]:
+                break
+            button = QtGui.QRadioButton(line)
+            if governor == line:
+                button.setChecked(True)
+            layout.addWidget(button, i, 0)
+            self.connect(button, QtCore.SIGNAL('toggled(bool)'),
+                         partial(self.buttonToggled, line))
+            layout.setRowStretch(i, 1)
+        if governor == "userspace":
+            with open(CPUFREQ + 'scaling_available_frequencies') as f:
+                self.freqs = f.read().split()
+            with open(CPUFREQ + 'scaling_cur_freq') as f:
+                cur_freq = f.read().strip()
+            slider = QtGui.QSlider(QtCore.Qt.Vertical)
+            slider.setMaximum(len(self.freqs) - 1)  # minimum defaults to 0
+            layout.addWidget(slider, 0, 1, len(governors) - 1, 1)
+            self.connect(slider, QtCore.SIGNAL('valueChanged(int)'),
+                         self.updateLabel)
 
-                self.label = QtGui.QLabel("%.1fGHz" % (float(cur_freq) / 1e6))
-                layout.addWidget(self.label, len(governors) - 1, 1)
+            self.label = QtGui.QLabel("%.1fGHz" % (float(cur_freq) / 1e6))
+            layout.addWidget(self.label, len(governors) - 1, 1)
+        if first:
             self.ui.groupBox.setLayout(layout)
 
     def updateLabel(self, value):
@@ -49,6 +64,13 @@ class CPUSlider(QtGui.QMainWindow):
             f.write(new_freq + '\n')
         self.label.setText("%.1fGHz" % (float(new_freq) / 1e6))
 
+    def buttonToggled(self, name, checked):
+        if checked:
+            # Probably unnecessary
+            #self.ui.statusbar.children()[1].setText(name)
+            with open(CPUFREQ + 'scaling_governor', 'w') as f:
+                f.write(name + '\n')
+            self.refresh()
 
 def main():
     app = QtGui.QApplication(sys.argv)
