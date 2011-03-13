5c65d3bc321b7de37e8b35dbbb13f5ff065d9f7b
Bruce Duncan <bwduncan@gmail.com>
2011-03-13 01:57:50 +0000

    Be more clever about updating the widgets.
    Splits refresh out into setup and refresh. The button deleting magic is still
    there but I can't test it.
    
    Disable the slider if the governor is not "userspace". Also hide the label.
    This makes the interface a bit clunky as the slider jumps about when the label
    is hidden. The alternative is to have a timer check the current frequency so
    that the label can be properly updated, but that's ugly.
    
    There are now a lot of object variables. Should probably fix that...

--- cpuslider.py
+++ cpuslider.py
@@ -14,60 +14,80 @@ class CPUSlider(QtGui.QMainWindow):
 
         self.ui = Ui_cpuSliderWindow()
         self.ui.setupUi(self)
+        self.setup()
+        self.refresh()
+
+    def setup(self):
         self.statusLabel = QtGui.QLabel()
         self.ui.statusbar.addWidget(self.statusLabel)
-        self.refresh(first=True)
-
-    def refresh(self, first=False):
         with open(CPUFREQ + 'scaling_governor') as f:
             governor = f.read().strip()
         self.statusLabel.setText(governor)
-        if first:
-            layout = QtGui.QGridLayout()
-        else:
-            layout = self.ui.groupBox.layout()
+        self.layout = QtGui.QGridLayout()
         with open(CPUFREQ + 'scaling_available_governors') as f:
             governors = f.read().split()
-        print layout.children()
-        for button in layout.children():
-            if button.text() not in governors:
-                layout.removeWidget(button)
+        self.buttons = []
         for i, line in enumerate(governors):
-            if line in [x.text() for x in layout.children()]:
-                break
             button = QtGui.QRadioButton(line)
             if governor == line:
                 button.setChecked(True)
-            layout.addWidget(button, i, 0)
+            self.layout.addWidget(button, i, 0)
+            self.layout.setRowStretch(i, 1)
             self.connect(button, QtCore.SIGNAL('toggled(bool)'),
                          partial(self.buttonToggled, line))
-            layout.setRowStretch(i, 1)
-        if governor == "userspace":
-            with open(CPUFREQ + 'scaling_available_frequencies') as f:
-                self.freqs = f.read().split()
-            with open(CPUFREQ + 'scaling_cur_freq') as f:
-                cur_freq = f.read().strip()
-            slider = QtGui.QSlider(QtCore.Qt.Vertical)
-            slider.setMaximum(len(self.freqs) - 1)  # minimum defaults to 0
-            layout.addWidget(slider, 0, 1, len(governors) - 1, 1)
-            self.connect(slider, QtCore.SIGNAL('valueChanged(int)'),
-                         self.updateLabel)
+            self.buttons.append(button)
+        with open(CPUFREQ + 'scaling_available_frequencies') as f:
+            self.freqs = f.read().split()
+        with open(CPUFREQ + 'scaling_cur_freq') as f:
+            cur_freq = f.read().strip()
+        self.slider = QtGui.QSlider(QtCore.Qt.Vertical)
+        self.slider.setMaximum(len(self.freqs) - 1)  # minimum defaults to 0
+        self.slider.setEnabled(governor == "userspace")
+        # The value will be set in refresh.
+        self.layout.addWidget(self.slider, 0, 1, len(governors) - 1, 1)
+        self.connect(self.slider, QtCore.SIGNAL('valueChanged(int)'),
+                     self.updateLabel)
 
-            self.label = QtGui.QLabel("%.1fGHz" % (float(cur_freq) / 1e6))
-            layout.addWidget(self.label, len(governors) - 1, 1)
-        if first:
-            self.ui.groupBox.setLayout(layout)
+        self.label = QtGui.QLabel("%.1fGHz" % (float(cur_freq) / 1e6))
+        self.label.setShown(governor == "userspace")
+        self.layout.addWidget(self.label, len(governors) - 1, 1)
+        self.ui.groupBox.setLayout(self.layout)
+
+    def refresh(self):
+        # Set the status bar to the current governor
+        with open(CPUFREQ + 'scaling_governor') as f:
+            governor = f.read().strip()
+        self.statusLabel.setText(governor)
+        self.slider.setEnabled(governor == "userspace")
+        with open(CPUFREQ + 'scaling_available_governors') as f:
+            governors = f.read().split()
+        # Remove governors which no longer exist
+        for button in self.buttons: # TODO Can't test this easily...
+            if button.text() not in governors:
+                button.deleteLater()
+                self.buttons.remove(button)
+        with open(CPUFREQ + 'scaling_available_frequencies') as f:
+            self.freqs = f.read().split()
+        # Set the slider bar to the current frequency.
+        with open(CPUFREQ + 'scaling_cur_freq') as f:
+            freq = f.read().strip()
+            # The label will be updated by the signal to updateLabel.
+            self.slider.setValue(len(freq) - self.freqs.index(freq) - 1)
+        self.label.setShown(governor == "userspace")
+
 
     def updateLabel(self, value):
         new_freq = self.freqs[-value - 1]
-        with open(CPUFREQ + 'scaling_setspeed', 'w') as f:
-            f.write(new_freq + '\n')
         self.label.setText("%.1fGHz" % (float(new_freq) / 1e6))
+        with open(CPUFREQ + 'scaling_governor') as f:
+            governor = f.read().strip()
+        if governor == "userspace":
+            with open(CPUFREQ + 'scaling_setspeed', 'w') as f:
+                f.write(new_freq + '\n')
+        self.label.setShown(governor == "userspace")
 
     def buttonToggled(self, name, checked):
         if checked:
-            # Probably unnecessary
-            #self.ui.statusbar.children()[1].setText(name)
             with open(CPUFREQ + 'scaling_governor', 'w') as f:
                 f.write(name + '\n')
             self.refresh()
