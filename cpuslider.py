0be2fcfe7bc85b4fc6e541d1de232ce6105957ea
Bruce Duncan <bwduncan@gmail.com>
2011-03-16 01:58:13 +0000

    Always show the slider and label. Fix a probable bug.
    The user should always be able to use the slider without having to set the
    governor first. Set the governor for them if they do this.
    
    len(freq) should have been len(self.freqs). Mostly, these were equal anyway!
    
    Also, try to do less in setup(), as it only has to be repeated in refresh().

--- cpuslider.py
+++ cpuslider.py
@@ -20,17 +20,12 @@ class CPUSlider(QtGui.QMainWindow):
     def setup(self):
         self.statusLabel = QtGui.QLabel()
         self.ui.statusbar.addWidget(self.statusLabel)
-        with open(CPUFREQ + 'scaling_governor') as f:
-            governor = f.read().strip()
-        self.statusLabel.setText(governor)
         self.layout = QtGui.QGridLayout()
         with open(CPUFREQ + 'scaling_available_governors') as f:
             governors = f.read().split()
         self.buttons = []
         for i, line in enumerate(governors):
             button = QtGui.QRadioButton(line)
-            if governor == line:
-                button.setChecked(True)
             self.layout.addWidget(button, i, 0)
             self.layout.setRowStretch(i, 1)
             self.connect(button, QtCore.SIGNAL('toggled(bool)'),
@@ -42,14 +37,12 @@ class CPUSlider(QtGui.QMainWindow):
             cur_freq = f.read().strip()
         self.slider = QtGui.QSlider(QtCore.Qt.Vertical)
         self.slider.setMaximum(len(self.freqs) - 1)  # minimum defaults to 0
-        self.slider.setEnabled(governor == "userspace")
         # The value will be set in refresh.
         self.layout.addWidget(self.slider, 0, 1, len(governors) - 1, 1)
         self.connect(self.slider, QtCore.SIGNAL('valueChanged(int)'),
                      self.updateLabel)
 
         self.label = QtGui.QLabel("%.1fGHz" % (float(cur_freq) / 1e6))
-        self.label.setShown(governor == "userspace")
         self.layout.addWidget(self.label, len(governors) - 1, 1)
         self.ui.groupBox.setLayout(self.layout)
 
@@ -58,7 +51,6 @@ class CPUSlider(QtGui.QMainWindow):
         with open(CPUFREQ + 'scaling_governor') as f:
             governor = f.read().strip()
         self.statusLabel.setText(governor)
-        self.slider.setEnabled(governor == "userspace")
         with open(CPUFREQ + 'scaling_available_governors') as f:
             governors = f.read().split()
         # Remove governors which no longer exist
@@ -66,25 +58,26 @@ class CPUSlider(QtGui.QMainWindow):
             if button.text() not in governors:
                 button.deleteLater()
                 self.buttons.remove(button)
+            elif button.text() == governor:
+                button.setChecked(True)
         with open(CPUFREQ + 'scaling_available_frequencies') as f:
             self.freqs = f.read().split()
         # Set the slider bar to the current frequency.
         with open(CPUFREQ + 'scaling_cur_freq') as f:
             freq = f.read().strip()
-            # The label will be updated by the signal to updateLabel.
-            self.slider.setValue(len(freq) - self.freqs.index(freq) - 1)
-        self.label.setShown(governor == "userspace")
-
+        # The label will be updated by the signal to updateLabel.
+        self.slider.setValue(len(self.freqs) - self.freqs.index(freq) - 1)
 
     def updateLabel(self, value):
         new_freq = self.freqs[-value - 1]
         self.label.setText("%.1fGHz" % (float(new_freq) / 1e6))
         with open(CPUFREQ + 'scaling_governor') as f:
             governor = f.read().strip()
-        if governor == "userspace":
-            with open(CPUFREQ + 'scaling_setspeed', 'w') as f:
-                f.write(new_freq + '\n')
-        self.label.setShown(governor == "userspace")
+        if governor != "userspace":
+            with open(CPUFREQ + 'scaling_governor') as f:
+                f.write("userspace\n")
+        with open(CPUFREQ + 'scaling_setspeed', 'w') as f:
+            f.write(new_freq + '\n')
 
     def buttonToggled(self, name, checked):
         if checked:
